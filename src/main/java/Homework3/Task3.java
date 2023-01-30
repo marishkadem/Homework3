dd0a2eb2520c79cdb969ea326dc4b16ff57b2609
Marina <marishka-2008-91@mail.ru>
2023-01-30 15:12:17 +0300

    Tasks

---
+++ src/main/java/Homework3/Task3.java
@@ -0,0 +1,51 @@
+package Homework3;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Task3 {
+    public static void main(String[] args){
+        List<String> planets = new ArrayList<>();
+        HashMap<String, Integer> planetCount = new HashMap<>();
+        Random rand = new Random();
+
+        String[] solarSystemPlanets = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+        for (int i = 0; i < 10; i++) {
+            int randIndex = rand.nextInt(solarSystemPlanets.length);
+            planets.add(solarSystemPlanets[randIndex]);
+        }
+
+        planets.add("Pluto");
+        planets.add("Pluto");
+        planets.add("Pluto");
+        planets.add("Pluto");
+
+/*        for (String planet : planets) {
+            System.out.printf("%s\t%s", planet, Collections.frequency(planets, planet));
+        }*/
+
+        for (String planet : planets) {
+            if (planetCount.containsKey(planet)) {
+                planetCount.put(planet, planetCount.get(planet) + 1);
+            } else {
+                planetCount.put(planet, 1);
+            }
+        }
+
+        for (String planet : planetCount.keySet()) {
+            System.out.printf("%s\t%s%n", planet, planetCount.get(planet));
+        }
+
+
+
+        planets.retainAll(Arrays.asList(solarSystemPlanets));
+        System.out.println(planets);
+
+        List<String> ResultList =
+                planets.stream().distinct().collect(Collectors.toList());
+        System.out.println(ResultList);
+
+
+
+    }
+}
+
